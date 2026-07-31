dc19f7361dced387a682f8c94f3339528d0080b4
Yingbai He <yingbai.he.dev@outlook.com>
2026-07-31 16:44:40 -0700

    release: v0.2.0

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
         in
         pkgs.stdenv.mkDerivation {
           pname = "agent-fleet";
-          version = "0.1.0";
+          version = "0.2.0";
           src = self;
 
           nativeBuildInputs = [ pkgs.makeWrapper ];
